fcc6a239ecb13095d545e61bad626c1b55585006
gitulisse <nason.ulisse@gmail.com>
2022-12-08 10:28:58 +0100

    ForgotPasswordActivity was substituted with ForgotPasswordFragment.

---
+++ app/src/main/java/com/example/projectnewsbgn/login/ForgotPasswordFragment.java
@@ -0,0 +1,16 @@
+package com.example.projectnewsbgn.login;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.projectnewsbgn.R;
+
+public class NewLoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
